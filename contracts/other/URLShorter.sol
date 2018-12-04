c01cf8f4c26f976615b41c7265e653b67240a32b
FriendlyUser <studentdavidli@gmail.com>
2018-12-03 20:27:17 -0800

    updating smart contract

--- contracts/other/URLShorter.sol
+++ contracts/other/URLShorter.sol
@@ -43,43 +43,4 @@ contract e0x {
       link.url
    );
   }
-event LinkAdded(uint linkId, string url);
- 
- struct LinkTemplate {
-  address userAddress;
-  string url;
- }
- 
- uint lastLinkId;
- mapping (uint => LinkTemplate) public linkMapping;
- 
- constructor() public {
-  lastLinkId = 0;
- }
- 
- 
- function createNewLink(string url) public returns (uint) {
-     lastLinkId++;
-  linkMapping[lastLinkId] = LinkTemplate(msg.sender, url);
-  emit LinkAdded(lastLinkId, url);
-  return lastLinkId;
- }
- 
- modifier linkExists(uint linkId) {
-     //link with the given hash does not exist
-  if(linkMapping[linkId].userAddress == 0x0000000000000000000000000000000000000000) {
-   revert();
-  }
-  _;
- }
- 
- function getLink(uint linkId) linkExists(linkId) public constant
-  returns(
-   address,
-   string
-  ) {
-      //emit LinkVisited(link.url,linkId);
-      LinkTemplate memory link = linkMapping[linkId];
-   return( link.userAddress,link.url);
-  }
- }
+}
